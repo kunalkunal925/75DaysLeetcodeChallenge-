e96eb9767a06626c5163d827633320ae5b408bce
Kunal Kumar <kunalmusic.34@gmail.com>
2026-04-02 07:16:56 +0530

    Time: 319 ms (39.33%), Space: 78.5 MB (20.34%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = {}  # key -> node
+        
+        # Dummy nodes for the doubly linked list
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        """Remove an existing node from the linked list."""
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def _add(self, node):
+        """Add a new node right after the dummy head."""
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key):
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add(node)
+            return node.value
+        return -1
+
+    def put(self, key, value):
+        if key in self.cache:
+            self._remove(self.cache[key])
+        
+        new_node = Node(key, value)
+        self._add(new_node)
+        self.cache[key] = new_node
+        
+        if len(self.cache) > self.capacity:
+            # Evict the least recently used (node before dummy tail)
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
