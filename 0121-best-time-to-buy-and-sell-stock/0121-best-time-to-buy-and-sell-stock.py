97dd04247fb363d653d0829e71d620148976aab1
Kunal Kumar <kunalmusic.34@gmail.com>
2026-04-10 21:02:53 +0530

    Time: 15 ms (98.56%), Space: 28.4 MB (97.37%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         min_price = float('inf')
         max_profit = 0
         
