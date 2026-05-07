d21d6a45b35b27fc6592c7e91a6c03f90a2424de
Kunal Kumar <kunalmusic.34@gmail.com>
2026-05-07 21:36:52 +0530

    Time: 263 ms (59.19%), Space: 17.1 MB (60.32%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def totalFruit(self, fruits):
+        """
+        :type fruits: List[int]
+        :rtype: int
+        """
+        count = {}
+        left = 0
+        max_fruits = 0
+        
+        for right in range(len(fruits)):
+            count[fruits[right]] = count.get(fruits[right], 0) + 1
+            
+            while len(count) > 2:
+                count[fruits[left]] -= 1
+                if count[fruits[left]] == 0:
+                    del count[fruits[left]]
+                left += 1
+            
+            max_fruits = max(max_fruits, right - left + 1)
+            
+        return max_fruits
