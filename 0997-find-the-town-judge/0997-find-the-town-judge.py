a4d4d23b15ec115c33d7c15fb6773b8f7137b68d
Kunal Kumar <kunalmusic.34@gmail.com>
2026-05-27 07:39:21 +0530

    Time: 11 ms (90.29%), Space: 15.9 MB (77.64%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findJudge(self, n, trust):
+        """
+        :type n: int
+        :type trust: List[List[int]]
+        :rtype: int
+        """
+        trust_scores = [0] * (n + 1)
+        
+        for a, b in trust:
+            trust_scores[a] -= 1
+            trust_scores[b] += 1
+            
+        for i in range(1, n + 1):
+            if trust_scores[i] == n - 1:
+                return i
+                
+        return -1
